33763a5d61a08c3184cfe3a2eecd0634c2b44c1b
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2022-11-15 18:20:17 +0530

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,12 +6,14 @@ import { IoReorderThreeOutline } from 'react-icons/io5';
 import { MdOutlineAccountCircle } from 'react-icons/md';
 import { AiFillYoutube, AiOutlineSearch } from 'react-icons/ai';
 
+import Notifications from './Notifications';
 import ChatBig from "./ChatBig";
 import Settings from './Setting';
 const Navbar = () => {
 
     const [showModal, setShowModal] = useState(false)
     const [showModal2, setShowModal2] = useState(false)
+    const [showModal3, setShowModal3] = useState(false)
 
     function close() {
         setShowModal(false)
@@ -19,17 +21,24 @@ const Navbar = () => {
     function close2() {
         setShowModal2(false)
     }
+    function close3() {
+        setShowModal3(false)
+    }
     function modalShow() {
         setShowModal(true)
     }
     function modal2Show() {
         setShowModal2(true)
     }
+    function modal3Show() {
+        setShowModal3(true)
+    }
     return (
         <>
             <div className='bg-gray-900 sticky top-0 z-50 flex items-center overflow-auto px-6 py-3 justify-between text-white right-0'>
                 <ChatBig onClose={close} visible={showModal} />
                 <Settings onClose={close2} visible={showModal2} />
+                <Notifications onClose={close3} visible={showModal3} />
                 <div className='flex items-center'>
                     <span className='cursor-pointer' onClick={modalShow}><IoReorderThreeOutline size="40px" /></span>
                     <Link to="/">
@@ -48,7 +57,7 @@ const Navbar = () => {
                     <Link to="/upload">
                         <BiVideoPlus className=' cursor-pointer mx-4' size="24px" />
                     </Link>
-                    <BsBell className=' cursor-pointer mx-4' size="24px" />
+                    <span onClick={modal3Show}><BsBell className=' cursor-pointer mx-4' size="24px" /></span>
                     <span onClick={modal2Show}><MdOutlineAccountCircle className=' cursor-pointer mx-4 mr-6' size="24px" /></span>
                 </div>
             </div>
